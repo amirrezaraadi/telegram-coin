7d62b3ae1f59b2bc39c4ad169dbc1e179b10742f
amirrezaraadi13 <amirrezaraadi13@yahoo.com>
2024-05-20 14:34:08 +0330

    refactoring one api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\EnergyController;
+use App\Http\Controllers\EnergyUpController;
+use App\Http\Controllers\Manager\UserController;
+use App\Http\Controllers\TrophyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,13 +12,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::prefix('manager')->name('manager')->group(function () {
-    Route::apiResource('/users' , \App\Http\Controllers\Manager\UserController::class);
-    Route::post('/users/add-wallet/{user}' , [\App\Http\Controllers\Manager\UserController::class , 'add_wallet'])->name('add-wallet');
-    Route::apiResource('/trophies' , \App\Http\Controllers\TrophyController::class);
-    Route::apiResource('/energy' , \App\Http\Controllers\EnergyController::class);
-    Route::apiResource('energy-up' , \App\Http\Controllers\EnergyUpController::class);
+    Route::apiResource('/users' , UserController::class);
+    Route::post('/users/add-wallet/{user}' , [UserController::class , 'add_wallet'])->name('add-wallet');
+    Route::apiResource('/trophies' , TrophyController::class);
+    Route::apiResource('/energy' , EnergyController::class);
+//    Route::apiResource('energy-up' , EnergyUpController::class);
 });
 Route::prefix('status')->name('status')->group(function () {
-    Route::put('/trophies/{trophy}' , [\App\Http\Controllers\TrophyController::class , 'default'])->name('default');
+    Route::put('/trophies/{trophy}' , [TrophyController::class , 'default'])->name('default');
 });
 
