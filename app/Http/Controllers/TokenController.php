c0af3e282bc1ac1d0e39d49554f5e507d8a2e8d3
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-05-25 09:02:30 +0330

    start token

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Token;
+use App\Http\Requests\StoreTokenRequest;
+use App\Http\Requests\UpdateTokenRequest;
+
+class TokenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTokenRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Token $token)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Token $token)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTokenRequest $request, Token $token)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Token $token)
+    {
+        //
+    }
+}
