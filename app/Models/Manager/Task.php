5ca396c3b5df8d18851b9e8fe9f775599df24ed7
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-05-28 13:45:02 +0330

    end  Task.php

--- app/Models/Manager/Task.php
+++ app/Models/Manager/Task.php
@@ -2,10 +2,25 @@
 
 namespace App\Models\Manager;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+        'link',
+        'amount',
+        'is_default',
+    ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
